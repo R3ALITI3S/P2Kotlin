59f5efeca7695c17a4802967fd1a0ed082e48ba2
R3ALITI3S <nfjo23@student.aau.dk>
2024-04-08 10:28:08 +0200

    Nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    // new
+    implementation ("androidx.navigation:navigation-compose:2.4.0-alpha04")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
