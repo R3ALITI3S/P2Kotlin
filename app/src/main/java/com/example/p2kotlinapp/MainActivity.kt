8fdbe4ed4350944e5235be7e27ce307aa1ff1473
R3ALITI3S <nfjo23@student.aau.dk>
2024-04-08 11:23:37 +0200

    Button work yes

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -9,89 +9,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.p2kotlinapp.R.string.tomatoplant
-import com.example.p2kotlinapp.ui.theme.P2KotlinAppTheme
-
+import androidx.navigation.Navigation
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            P2KotlinAppTheme {
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
-            }
+            Navigation()
         }
-    }
-}
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.fillMaxSize()
-    ){
-        Button(
-            modifier = Modifier
-                .height(48.dp)
-                .width(280.dp)
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 4.dp),
-            enabled = true,
-            onClick = { Log.d("tag","tesy")}
-        ) {
-            Text(text = (stringResource(id = R.string.waterplant)), modifier = Modifier)
-        }
-
-        Button(
-            modifier = Modifier
-                .height(96.dp)
-                .width(360.dp)
-                .align(Alignment.TopCenter)
-                .padding(bottom = 4.dp),
-            enabled = true,
-            onClick = { Log.d("tag","tesy")}
-        ) {
-            Text(text = (stringResource(id = R.string.waterlevel)), modifier = Modifier)
-        }
-    }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize()
-
-
-    ) {
-        Image(
-            painter = painterResource(R.drawable.tomatoplant),
-            contentDescription = "tomatoplant",
-            modifier = Modifier
-                .wrapContentSize()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = stringResource(tomatoplant), fontSize = 18.sp)
     }
 }
--- app/src/main/java/com/example/p2kotlinapp/Navigation.kt
+++ app/src/main/java/com/example/p2kotlinapp/Navigation.kt
@@ -20,44 +20,48 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun navController() {
+fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
         composable(route = Screen.MainScreen.route) {
-            Screen.MainScreen(navController = navController)
+            MainScreen(navController = navController)
         }
         composable(
-            route = Screen.DetailScreen.route,
-            arguments = listOf()
-        )
+            route = Screen.DetailScreen.route
+        ) { entry ->
+            DetailScreen(name = entry.arguments?.getString("name"))
+        }
     }
 }
 
 @Composable
-fun Screen.MainScreen(navController: NavController) {
-    var text by remember {
+fun MainScreen(navController: NavController) {
+    /* var text by remember {
         mutableStateOf("")
-    }
+    } */
     Column (
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 50.dp)
     ) {
-        TextField(value = text, onValueChange = {
+        /*TextField(value = text, onValueChange = {
             text = it
         },
             modifier = Modifier.fillMaxWidth()
-        )
+        ) */
         Spacer(modifier = Modifier.height(8.dp))
         Button(
             onClick = {
-                navController.navigate(Screen.DetailScreen.route)
+                navController.navigate(Screen.DetailScreen.route) //withArgs(text) could be used
             },
             modifier = Modifier.align(Alignment.End)
         ) {
@@ -72,7 +76,6 @@ fun DetailScreen(name:String?) {
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Hello, $name")
+        Text(text = "Hello")
     }
-
 }
