1a99f17c2173bcfd09f43ab20331b503a5e38d8c
R3ALITI3S <nfjo23@student.aau.dk>
2024-04-08 10:28:08 +0200

    Nav

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+buildscript {
+    /* ext {
+
+        // compose_version = '1.0.0-beta09'
+    } */
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        // Fail if standing - classpath("com.android.tools.build.gradle:7.0.0-beta05")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+    }
+}
+
