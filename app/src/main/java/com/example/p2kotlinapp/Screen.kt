5c5f8adce9a4fb9b1fc9363e2bf1ee185df5afc2
R3ALITI3S <nfjo23@student.aau.dk>
2024-04-08 11:29:52 +0200

    Cut alot of the unused code out - Still works tho :))

--- app/src/main/java/com/example/p2kotlinapp/Screen.kt
+++ app/src/main/java/com/example/p2kotlinapp/Screen.kt
@@ -3,13 +3,4 @@ package com.example.p2kotlinapp
 sealed class Screen(val route: String) {
     object MainScreen : Screen("main_screen")
     object DetailScreen : Screen("detail_screen")
-
-    fun withArgs(vararg args: String): String {
-        return buildString {
-            append(route)
-            args.forEach { arg ->
-                append("/$arg")
-            }
-        }
-    }
 }
