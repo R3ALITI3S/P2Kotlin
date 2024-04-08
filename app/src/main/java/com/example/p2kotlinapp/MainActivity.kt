b069c51c0b3a53681a4d68100496ae580a2c2209
R3ALITI3S <nfjo23@student.aau.dk>
2024-04-08 14:12:30 +0200

    Now with a bottom bar

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.p2kotlinapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.Navigation
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ScaffoldExample(modifier = Modifier())
             Navigation()
         }
     }
