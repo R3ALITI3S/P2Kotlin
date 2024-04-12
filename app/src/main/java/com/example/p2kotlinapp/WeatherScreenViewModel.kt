962d51f54bb81071a0c27261569cc3434e222b0c
AJ <151622122+ajohan23@users.noreply.github.com>
2024-04-12 16:11:23 +0200

    Implemented different states to the weather screen

--- app/src/main/java/com/example/p2kotlinapp/WeatherScreenViewModel.kt
+++ app/src/main/java/com/example/p2kotlinapp/WeatherScreenViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.p2kotlinapp
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.p2kotlinapp.network.WeatherApi
@@ -8,22 +12,26 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 class WeatherScreenViewModel: ViewModel() {
 
-    private val _state = MutableStateFlow(WeatherScreenState())
-    val state: StateFlow<WeatherScreenState> = _state.asStateFlow()
-
+    var _state: WeatherScreenState by mutableStateOf(WeatherScreenState.Loading)
+        private set
+    val state = _state
     init {
         getCurrentWeatherData()
     }
 
     private fun getCurrentWeatherData() {
         viewModelScope.launch {
-            val listResult = WeatherApi.retrofitService.getCurrentWeather()
-            _state.update {it.copy(
-                fullResponse = listResult
-            )}
+            _state = try {
+                val result = WeatherApi.retrofitService.getCurrentWeather()
+                WeatherScreenState.Succes(result)
+            }
+            catch (e :IOException) {
+                WeatherScreenState.Error
+            }
         }
     }
 }
