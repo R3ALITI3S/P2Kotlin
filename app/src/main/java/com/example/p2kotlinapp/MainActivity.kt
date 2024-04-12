4f40cb9c1037eda2664ee95eaf6438bdf2456434
AJ <151622122+ajohan23@users.noreply.github.com>
2024-04-12 13:38:58 +0200

    Added state as dependency for WeatherScreen and added an vWeatherScreenViewModel instance to MainActivity

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -3,18 +3,30 @@ package com.example.p2kotlinapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.p2kotlinapp.ui.theme.P2KotlinAppTheme
 
 class MainActivity : ComponentActivity() {
 
-    
+    private val viewModel by viewModels<WeatherScreenViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return WeatherScreenViewModel() as T
+                }
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +37,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WeatherScreen()
+                    val state = viewModel.state.collectAsState()
+                    WeatherScreen(state)
                 }
             }
         }
