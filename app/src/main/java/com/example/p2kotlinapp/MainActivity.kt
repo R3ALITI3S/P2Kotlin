25be7924cb603e59cab3d50496934ea598d4ea0b
AJ <151622122+ajohan23@users.noreply.github.com>
2024-04-12 14:10:48 +0200

    Added internet permission

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
@@ -37,7 +38,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val state = viewModel.state.collectAsState()
+                    viewModel
+
+                    val state by viewModel.state.collectAsState()
                     WeatherScreen(state)
                 }
             }
--- app/src/main/java/com/example/p2kotlinapp/WeatherScreenViewModel.kt
+++ app/src/main/java/com/example/p2kotlinapp/WeatherScreenViewModel.kt
@@ -6,6 +6,7 @@ import com.example.p2kotlinapp.network.WeatherApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class WeatherScreenViewModel: ViewModel() {
@@ -13,10 +14,14 @@ class WeatherScreenViewModel: ViewModel() {
     private val _state = MutableStateFlow(WeatherScreenState())
     val state: StateFlow<WeatherScreenState> = _state.asStateFlow()
 
+    init {
+        getCurrentWeatherData()
+    }
+
     private fun getCurrentWeatherData() {
         viewModelScope.launch {
             val listResult = WeatherApi.retrofitService.getCurrentWeather()
-            _state
+            _state.update()
         }
     }
 }
