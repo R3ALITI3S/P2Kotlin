e6814ad7736ba088e217d96c3832b3043b8a8d9a
Nicolatian <nicolaivestergaard2@gmail.com>
2024-04-12 13:19:12 +0200

    Button!
    Water the plant

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -1,46 +1,65 @@
 package com.example.p2kotlinapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.p2kotlinapp.R.string.waterplant
 import com.example.p2kotlinapp.ui.theme.P2KotlinAppTheme
 
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             P2KotlinAppTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingPreview()
                 }
             }
         }
     }
 }
-
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    P2KotlinAppTheme {
-        Greeting("Weather report!")
+fun GreetingPreview(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier.fillMaxSize()
+    ){
+        Button(
+            modifier = Modifier
+                .height(48.dp)
+                .width(280.dp)
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 4.dp),
+            enabled = true,
+            onClick = { Log.d("tag","tesy")}
+        ) {
+            Text(text = (stringResource(id = R.string.waterplant)), modifier = Modifier)
+        }
     }
 }
