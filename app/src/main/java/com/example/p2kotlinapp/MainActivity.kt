bc2141aae41bebda373e97ec97a460bfd47920fb
Laerkeals <lba9210@gmail.com>
2024-04-12 14:24:49 +0200

    image

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.p2kotlinapp
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,11 +21,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.p2kotlinapp.R.string.waterplant
+import androidx.compose.ui.unit.sp
+import com.example.p2kotlinapp.R.string.tomatoplant
 import com.example.p2kotlinapp.ui.theme.P2KotlinAppTheme
 
 
@@ -61,5 +64,22 @@ fun GreetingPreview(modifier: Modifier = Modifier) {
         ) {
             Text(text = (stringResource(id = R.string.waterplant)), modifier = Modifier)
         }
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.tomatoplant),
+            contentDescription = "tomatoplant",
+            modifier = Modifier
+                .wrapContentSize()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = stringResource(tomatoplant), fontSize = 18.sp)
     }
 }
