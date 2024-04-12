a21273d51ec40b226599ec4890339634ebfe062b
R3ALITI3S <nfjo23@student.aau.dk>
2024-04-12 16:24:49 +0200

    does it work?

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -4,15 +4,98 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import java.lang.reflect.Modifier
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.p2kotlinapp.R.string.tomatoplant
+import com.example.p2kotlinapp.ui.theme.P2KotlinAppTheme
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ScaffoldExample(modifier = Modifier())
-            ScaffoldExample()
-            Navigation()
+
+            P2KotlinAppTheme {
+
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = androidx.compose.ui.Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                    ScaffoldExample()
+                    Navigation()
+                }
+            }
         }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Box(modifier = androidx.compose.ui.Modifier.fillMaxSize()
+    ){
+        Button(
+            modifier = androidx.compose.ui.Modifier
+                .height(48.dp)
+                .width(280.dp)
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 4.dp),
+            enabled = true,
+            onClick = { Log.d("tag","tesy")}
+        ) {
+            Text(text = (stringResource(id = R.string.waterplant)), modifier = androidx.compose.ui.Modifier)
+        }
+
+        Button(
+            modifier = androidx.compose.ui.Modifier
+                .height(96.dp)
+                .width(360.dp)
+                .align(Alignment.TopCenter)
+                .padding(bottom = 4.dp),
+            enabled = true,
+            onClick = { Log.d("tag","tesy")}
+        ) {
+            Text(text = (stringResource(id = R.string.waterlevel)), modifier = androidx.compose.ui.Modifier)
+        }
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = androidx.compose.ui.Modifier
+            .fillMaxSize()
+
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.tomatoplant),
+            contentDescription = "tomatoplant",
+            modifier = androidx.compose.ui.Modifier
+                .wrapContentSize()
+        )
+        Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+        Text(text = stringResource(tomatoplant), fontSize = 18.sp)
     }
 }
