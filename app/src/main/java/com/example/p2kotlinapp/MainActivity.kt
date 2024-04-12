115a8a0d7c3b1a84c760114998ed4b0ab490cc5e
AJ <151622122+ajohan23@users.noreply.github.com>
2024-04-12 16:25:43 +0200

    Added some debugging prints to WeatherScreenViewModel

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -38,8 +38,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    viewModel
-
                     val state by viewModel.state.collectAsState()
                     WeatherScreen(state)
                 }
