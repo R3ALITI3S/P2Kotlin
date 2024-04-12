cef9e5420fb879f2d5a41dff64bb4a60b50bca89
AJ <151622122+ajohan23@users.noreply.github.com>
2024-04-12 13:38:58 +0200

    Added state as dependency for WeatherScreen and added an vWeatherScreenViewModel instance to MainActivity

--- app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/p2kotlinapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,6 +21,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,13 +29,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.p2kotlinapp.R.string.tomatoplant
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.p2kotlinapp.ui.theme.P2KotlinAppTheme
 
 
 class MainActivity : ComponentActivity() {
 
-
-    
+    private val viewModel by viewModels<WeatherScreenViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return WeatherScreenViewModel() as T
+                }
+            }
+        }
+    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +58,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Navigation()
+                    val state = viewModel.state.collectAsState()
                 }
             }
         }
--- app/src/main/java/com/example/p2kotlinapp/WeatherScreen.kt
+++ app/src/main/java/com/example/p2kotlinapp/WeatherScreen.kt
@@ -5,12 +5,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun WeatherScreen() {
+fun WeatherScreen(state: WeatherScreenState) {
 
     Column {
-        Text(text = "Rain: ")
-        Text(text = "Clouds: ")
-        Text(text = "Is Day: ")
-        Text(text = "Full Response: ")
+        Text(text = "Rain: ${state.rain}")
+        Text(text = "Clouds: ${state.cloudy}")
+        Text(text = "Is Day: ${state.isDay}")
+        Text(text = "Full Response: \n${state.fullResponse}")
     }
 }
